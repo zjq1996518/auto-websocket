607a59bcce80a6130915d20dd9fa5391f4c8dc21
九天厉魂 <zjq2133318@163.com>
2020-04-27 18:46:42 +0800

    websocket 并发

--- src/logger.py
+++ src/logger.py
@@ -6,9 +6,10 @@ PATH = f'{os.path.dirname(__file__)}/log'
 DEBUG = False
 
 
-def get_logger(level='warning', debug=DEBUG):
+def get_logger(path=None, level='warning', debug=DEBUG):
     """
     根据日期自动切分日志
+    :param path: 日志路径
     :param level: 日志打印级别
     :param debug: 开启debug则日志打印到控制台
     :return: 日志打印对象
@@ -20,7 +21,11 @@ def get_logger(level='warning', debug=DEBUG):
     if not os.path.exists(PATH):
         os.makedirs(PATH)
 
-    log_file = f'{PATH}/{datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d")}.log'
+    if path is None:
+        log_file = f'{PATH}/{datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d")}.log'
+    else:
+        log_file = path
+
     log_format = '%(asctime)s[%(levelname)s]: %(filename)s[%(lineno)d]: %(message)s'
     if debug:
         logging.basicConfig(level=level, format=log_format)
--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ from gevent.pywsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
 from src.auto_write import AutoWrite
 from src.config import *
+from src.logger import get_logger, PATH
 
 auto_write = AutoWrite(ADMIN_NAME, WEBSOCKET_LIST)
+
+
+logger = get_logger(f'{PATH}/gevent.log')
+err_logger = get_logger(f'{PATH}/gevent-err.log')
+
 
 if __name__ == '__main__':
     from src.websocket import app
@@ -15,7 +21,6 @@ if __name__ == '__main__':
             'keyfile': SSL_KEY_PATH,
             'certfile': SSL_CRT_PATH
         }
-
-    server = WSGIServer(('0.0.0.0', PORT), app, handler_class=WebSocketHandler, **ssl_parm)
+    server = WSGIServer(('0.0.0.0', PORT), backlog=1024, log=logger, error_log=err_logger, application=app, handler_class=WebSocketHandler, **ssl_parm)
     print("服务已启动")
     server.serve_forever()
